3697aa202e4d15dbc2c03f7165b878ffc02fd46d
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2024-03-11 00:26:06 -0400

    feat: header added

--- push_frontend/src/App.js
+++ push_frontend/src/App.js
@@ -4,27 +4,46 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="flex">
-      <div className="bg-white text-black w-64 min-h-screen p-5">
-        {/* Logo and Icon */}
-        <div className='flex'>
-          <img src="/logo.svg" alt="Push Logo" className='h-[30px] w-[30px] mr-3' />
-          <h1 className="text-2xl font-bold mb-10">Push</h1>
-        </div>
+    <div className='flex h-screen bg-[#F4F6F9]'>
+      {/* Sidebar Nav */}
+      <div className="hidden md:hidden lg:block">
+        <div className="bg-white text-black w-64 min-h-screen p-5">
+          {/* Logo and Icon */}
+          <div className='flex mb-10'>
+            <img src="/logo.svg" alt="Push Logo" className='h-[30px] w-[30px] mr-3' />
+            <h1 className="text-2xl font-bold">Push</h1>
+          </div>
 
-        {/* Dashboard Menu */}
-        <ul>
-          <li className="mb-5">
-            <a href="#employees" className="block hover:bg-[#E2E9F2]">Employees</a>
-          </li>
-          <li className="mb-5">
-            <a href="#scheduler" className="block hover:bg-[#E2E9F2]">Scheduler</a>
-          </li>
-          <li className="mb-5">
-            <a href="#timetracking" className="block hover:bg-[#E2E9F2]">Timetracking</a>
-          </li>
-        </ul>
+          {/* Dashboard Menu */}
+          <ul>
+            <li className="mb-5">
+              <a href="#employees" className="block hover:bg-[#E2E9F2]">Employees</a>
+            </li>
+            <li className="mb-5">
+              <a href="#scheduler" className="block hover:bg-[#E2E9F2]">Scheduler</a>
+            </li>
+            <li className="mb-5">
+              <a href="#timetracking" className="block hover:bg-[#E2E9F2]">Timetracking</a>
+            </li>
+          </ul>
+        </div>
       </div>
+      {/* Header */}
+      <div className='flex-1 flex-col'>
+        <div className="flex justify-between items-center bg-white p-4 ">
+          <h2 className="text-2xl font-bold">Employees</h2>
+          <div className="flex items-center space-x-3">   
+            <div className="flex items-center justify-center bg-[#68BEE2] rounded-full h-10 w-10 text-white font-extrabold">
+              JS 
+            </div>
+            <div>
+              <div className='font-extrabold text-[#0D325F]'>John Smith</div>
+              <div className='font-light text-[#61748F]'> johnsmith@gmail.com</div>
+            </div>
+          </div>         
+        </div>
+      </div>
+      {/* End */}
     </div>
   )
 }
