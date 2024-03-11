a8b5e9ce3f771a08d0918651618e7b193c042dfc
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2024-03-10 23:33:55 -0400

    feat: nav menu created

--- push_frontend/src/App.js
+++ push_frontend/src/App.js
@@ -1,14 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="flex">
+      <div className="bg-white text-black w-64 min-h-screen p-5">
+        {/* Logo and Icon */}
+        <div className='flex'>
+          <img src="/logo.svg" alt="Push Logo" className='h-[30px] w-[30px] mr-3' />
+          <h1 className="text-2xl font-bold mb-10">Push</h1>
+        </div>
+
+        {/* Dashboard Menu */}
+        <ul>
+          <li className="mb-5">
+            <a href="#employees" className="block hover:bg-[#E2E9F2]">Employees</a>
+          </li>
+          <li className="mb-5">
+            <a href="#scheduler" className="block hover:bg-[#E2E9F2]">Scheduler</a>
+          </li>
+          <li className="mb-5">
+            <a href="#timetracking" className="block hover:bg-[#E2E9F2]">Timetracking</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
