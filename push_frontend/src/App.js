74a6e35ea269b7b976ab64846ecdc99fb0f8fbbd
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2024-03-11 01:41:35 -0400

    wip: search bar and static cards created

--- push_frontend/src/App.js
+++ push_frontend/src/App.js
@@ -28,8 +28,11 @@ function App() {
           </ul>
         </div>
       </div>
-      {/* Header */}
+
+      
       <div className='flex-1 flex-col'>
+
+        {/* Header */}
         <div className="flex justify-between items-center bg-white p-4 ">
           <h2 className="text-2xl font-bold">Employees</h2>
           <div className="flex items-center space-x-3">   
@@ -42,7 +45,38 @@ function App() {
             </div>
           </div>         
         </div>
+
+        {/* Dashboard */}
+        <div className='w-full'>
+          <div className='flex p-8'>
+            <input className="w-2/3 px-4 py-2 border" placeholder="Filter employees by name" type="search" />
+          </div>
+        </div>
+
+        <div className="w-2/3 p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+
+          <div className="bg-white rounded shadow p-4 flex justify-between items-center">
+            <div>
+              <div className="font-medium text-lg">John Smith</div>
+              <div className="text-gray-600">Total hours</div>
+            </div>
+            <div className="text-gray-800 font-semibold">20.6</div>
+          </div>
+
+          <div className="bg-white rounded shadow p-4 flex justify-between items-center">
+            <div>
+              <div className="font-medium text-lg">John Smith</div>
+              <div className="text-gray-600">Total hours</div>
+            </div>
+            <div className="text-gray-800 font-semibold">20.6</div>
+          </div>
+
+        </div>
+
       </div>
+
+      
+
       {/* End */}
     </div>
   )
