137d77e348f4a22ad2c4ac273fea1f0ec9d9500c
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2024-03-11 20:39:09 -0400

    wip: cards now pulling information from json

--- push_frontend/src/components/Dashboard.js
+++ push_frontend/src/components/Dashboard.js
@@ -1,7 +1,17 @@
 import React from "react";
 import '../App.css'
+import data from '../employee_data.json'
+
+
 
 const Dashboard = () => {
+
+    const employeeInfo = data.map(employee => ({
+        id: employee.employee_id,
+        name: employee.first_name + ' ' + employee.last_name,
+        totalHours: employee.labour.reduce((sum, hour) => sum + hour.total, 0) //where sum starts at 0 per user
+    }))
+
     return(
         <div className='w-full'>
           <div className='flex p-8'>
@@ -9,22 +19,19 @@ const Dashboard = () => {
           </div>
           <div className="w-2/3 p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
 
-            <div className="bg-white rounded shadow p-4 flex justify-between items-center">
-              <div>
-                <div className="font-medium text-lg">John Smith</div>
-                <div className="text-gray-600">Total hours</div>
-              </div>
-              <div className="text-gray-800 font-semibold">20.6</div>
-            </div>
-
-            <div className="bg-white rounded shadow p-4 flex justify-between items-center">
-              <div>
-                <div className="font-medium text-lg">John Smith</div>
-                <div className="text-gray-600">Total hours</div>
-              </div>
-              <div className="text-gray-800 font-semibold">20.6</div>
-            </div>
-
+            {
+                employeeInfo.map((employee) => (
+                    <div key={employee.id} className="bg-white rounded shadow p-4 flex justify-between items-center">
+                        <div>
+                            <div className="font-extrabold text-lg">{employee.name}</div>
+                            <div className="text-gray-600 font-bold">Total hours</div>
+                        </div>
+                        
+                        <div className="text-gray-800 font-semibold pt-8">{employee.totalHours}</div>
+                    </div>
+                ))
+            }
+            
           </div>
         </div>
     )
